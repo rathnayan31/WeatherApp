be7bdc7be3c5dfa3c04319a8d763a2b61f1e649d
Rasmi <rasmi@Rasmis-MacBook-Air.local>
2018-05-05 13:00:08 +0530

    First commit

---
+++ app/src/main/java/com/mob/weatherapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.mob.weatherapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fun main(args : Array<String>){
+
+            println("Hello World !")
+        }
+    }
+}
